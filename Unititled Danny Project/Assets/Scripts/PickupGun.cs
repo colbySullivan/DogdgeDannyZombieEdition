fd715dfc76c5795f8d9913740fc5335eea0fc996
Colby Sullivan <sullivanc@southwestern.edu>
2024-05-16 00:34:47 -0500

    Rifle station

---
+++ Unititled Danny Project/Assets/Scripts/PickupGun.cs
@@ -0,0 +1,17 @@
+using UnityEngine.Events;
+using UnityEngine;
+
+public class PickupGun : MonoBehaviour
+{
+    public UnityEvent pickUp;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit " + collision.transform.name);
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit " + collision.transform.name);
+            pickUp?.Invoke();
+        }
+    }
+}
